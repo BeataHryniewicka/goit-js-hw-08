09c64f0244206432e57cfb344865052b39c9a48e
BeataHryniewicka <118068394+BeataHryniewicka@users.noreply.github.com>
2023-04-05 15:48:11 +0200

    .

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -9,13 +9,12 @@ const galleryArray = document.querySelector(`.gallery`);
 const newImages = galleryItems
   .map(
     image =>
-      //Link do oryginalnego obrazka powinien być przechowywany w atrybucie data source w elemencie <img>, oraz w href linku.
-      `<li class="gallery__item">
+      `<li>
   <a class="gallery__link" href="${image.original}">
     <img
       class="gallery__image"
       src="${image.preview}"
-      data-source="${image.original}"
+   
       alt="${image.description}"
     />
   </a>
@@ -25,23 +24,10 @@ const newImages = galleryItems
 
 galleryArray.insertAdjacentHTML('beforeend', newImages);
 
-galleryArray.addEventListener('click', ev => {
-  //zablokowanie przekierowania na stronę
-  ev.preventDefault();
-
-  //Otworzenie okna modalnego po kliknięciu w element galerii.
-  const instance = new SimpleLightbox(
-    `<img src="${ev.target.dataset.source}  ">`
-  );
-
-  const close = document.addEventListener('keydown', event => {
-    if (event.key === 'Escape') {
-      instance.close();
-      document.removeEventListener(Object, close);
-    }
-  });
-
-  instance.show();
-});
-
 console.log(galleryItems);
+
+// kliknięcie w obrazki, otwieranie i zamykanie okna modalnego oraz przewijaniem obrazków za pomocą klawiatury.
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
