a7a7f089681807fcfac32c1e948d31c049873c5a
BeataHryniewicka <118068394+BeataHryniewicka@users.noreply.github.com>
2023-04-05 13:52:24 +0200

    import SimpleLightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,55 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryArray = document.querySelector(`.gallery`);
+
+const newImages = galleryItems
+  .map(
+    image =>
+      //Link do oryginalnego obrazka powinien być przechowywany w atrybucie data source w elemencie <img>, oraz w href linku.
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</li>`
+  )
+  .join(' ');
+
+galleryArray.insertAdjacentHTML('beforeend', newImages);
+
+galleryArray.addEventListener('click', ev => {
+  //zablokowanie przekierowania na stronę
+  ev.preventDefault();
+  // if (nodeName == !"img") {
+  //   return;
+  // }
+  //Otworzenie okna modalnego po kliknięciu w element galerii.
+  const instance = basicLightbox.create(
+    `<img src="${ev.target.dataset.source}  ">`
+  );
+  // {
+  //   onShow: (instance) => {
+  //     window.addEventListener("keydown", (ev) => {
+  //       console.log(ev.code);
+  //       if (ev.code === 'Escape')
+  //         instance.close())
+  //   }
+  const close = document.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      instance.close();
+      document.removeEventListener(Object, close);
+    }
+  });
+
+  instance.show();
+});
 
 console.log(galleryItems);
