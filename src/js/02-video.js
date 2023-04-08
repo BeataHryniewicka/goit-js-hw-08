9b1d9999cec5701069f67d9f9489924cb2c2f7cb
BeataHryniewicka <118068394+BeataHryniewicka@users.noreply.github.com>
2023-04-08 22:52:36 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const vimeoEL = document.getElementById('vimeo-player');
+const player = new Player(vimeoEL);
+// const throttle = require('lodash.throttle');
+
+const onPlay = throttle(data => {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}, 1000);
+
+player.on('timeupdate', onPlay);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
