e02353e06cd87e5708364ecc8fb87dd82a7c532f
BeataHryniewicka <118068394+BeataHryniewicka@users.noreply.github.com>
2023-04-13 21:34:25 +0200

    .

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,11 +3,22 @@
 const throttle = require('lodash.throttle');
 const btnEl = document.querySelector('button[type = "submit"]');
 const formEl = document.querySelector('.feedback-form');
-const emailEl = document.querySelector('input[type = "email"]');
+const emailEl = document.querySelector('input[name = "email"]');
 const messageEl = document.querySelector('textarea[name = "message"]');
+// obiekt z polami email i message
+
+//funkcja odczytująca i zapisująca zmiany
+function updateForm() {
+  const updateItem = JSON.parse(
+    localStorage.getItem(`feedback-form-state` || '')
+  );
+  emailEl.value = updateItem.email || '';
+  messageEl.value = updateItem.message || '';
+}
+updateForm();
 
 const fillForm = () => {
-  // obiekt z polami email i message,
+  // obiekt z polami email i message
   const data = {
     email: emailEl.value,
     message: messageEl.value,
@@ -16,29 +27,14 @@ const fillForm = () => {
   localStorage.setItem('feedback-form-state', JSON.stringify(data));
 };
 
-//funkcja odczytująca i zapisująca zmiany
-const updateForm = () => {
-  const updateItem = JSON.parse(
-    localStorage.getItem(`feedback-form-state` || '')
-  );
-  emailEl.value = updateItem.email || '';
-  messageEl.value = updateItem.message || '';
-};
-//funkcaj usuwająca dane z local storage, czyszcząca formularz, drukujaca pusty juz obiekt data
-const clearForm = ev => {
+//funkcja drukujaca obiekt data, usuwająca dane z local storage, czyszcząca formularz
+function clearForm(ev) {
   ev.preventDefault();
   console.log(data);
   localStorage.clear();
-  //   removeItem('feedback-form-state');
+  //  removeItem('feedback-form-state');
   formEl.reset();
-};
-// const submit = e => {
-//   e.preventDefault();
-//   console.log(data);
-//   localStorage.clear();
-//   formEl.reset();
-// };
+}
 
-updateForm();
 formEl.addEventListener('input', throttle(fillForm, 500));
 btnEl.addEventListener('submit', clearForm);
